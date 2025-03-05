b55cab571b8cabad62c4694a7822f6b500d028de
Kenza-sh <kenza@goodman-ai.com>
2025-03-05 15:55:45 +0100

    Update __init__.py

--- youtube_search_anatomy/__init__.py
+++ youtube_search_anatomy/__init__.py
@@ -4,6 +4,7 @@ from youtube_search import YoutubeSearch
 from gliner import GLiNER
 import re
 import json
+import requests
 
 class AnatomieVideoSearch:
     def __init__(self):
@@ -14,7 +15,8 @@ class AnatomieVideoSearch:
         self.chaines_valides = ["Anat' Academy", "Carl Gdt (Anat' To Me)", "Docteur Par Coeur",
                                 "Dr Explique", "scandium theory", "Docteur C", "Promed Anatomie (Dr Khaled ANNABI)",
                                 "RB Physio", "Anatomie 3D Lyon", "Urologie fonctionnelle - Anatomie",
-                                "Encyclopédie Médicale", "Médecine Créative","Physio explain","Anatomyc"]
+                                "Encyclopédie Médicale", "Médecine Créative","Physio explain","Anatomyc",
+                               "Dave Bélanger","Ronald Déry","Anatomie AKIVI","RAKOTOZAFY Mahefa Serge (MÉDECINE EN VIDÉO)"]
         
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
         logging.info("Modèle GLiNER chargé avec succès.")
@@ -34,7 +36,39 @@ class AnatomieVideoSearch:
         except Exception as e:
             logging.error(f"Erreur lors de l'extraction des entités : {e}")
             return []
-
+    def recherche_images_wikipedia(self,entities):
+      """Recherche des images sur Wikipédia en fonction des entités anatomiques extraites avec des filtres sur la qualité et la source des images."""
+      image_links = []
+      try:
+          for entité in entities:
+                  url = f"https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages|pageterms&titles={entité}&pithumbsize=1500"
+                  response = requests.get(url).json()
+                  
+                  pages = response.get("query", {}).get("pages", {})
+                  
+                  for page_id, page_data in pages.items():
+                      if "thumbnail" in page_data:
+                          image_url = page_data["thumbnail"]["source"]
+                          
+                          # Vérifier si l'image provient de Wikimedia Commons
+                          if "commons" in image_url:
+                              width = page_data["thumbnail"].get("width", 0)
+                              
+                              # Vérifier la qualité et la pertinence de l'image
+                              if width >= 500:
+                                if 'svg' in image_url :
+                                      image_links.append(image_url)
+                              else:
+                                  logging.warning(f"Image ignorée (trop petite) : {image_url}")
+                          else:
+                              logging.warning(f"Image ignorée (non issue de Wikimedia) : {image_url}")
+              
+          logging.info(f"Images trouvées : {image_links}")
+          return list(set(image_links))  # Suppression des doublons
+      except Exception as e:
+          logging.error(f"Erreur lors de la recherche des images Wikipédia : {e}")
+          return []
+          
     def recherche_video(self, entities):
         """Recherche des vidéos YouTube en fonction des entités anatomiques extraites."""
         filtered_results = []
@@ -78,9 +112,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         
         entites = searcher.get_entities(query)
         videos = searcher.recherche_video(entites)
-
+        images=  searcher.recherche_images_wikipedia(entites)
         return func.HttpResponse(
-            json.dumps({"response": videos}),
+            json.dumps({"videos": videos , "images":images}),
             mimetype="application/json"
         )
 
